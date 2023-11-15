10b87d0e57f77d3702db2a6d61752f2f93655a37
hjxxzk <273017@student.pwr.edu.pl>
2023-11-15 14:31:57 +0100

    Łączy się z bazą danych, poprawnie wyświetla ui

---
+++ Laboratorium_3/src/main/java/logic/SQLiteWorker.java
@@ -0,0 +1,53 @@
+package logic;
+
+import opinion.Opinion;
+import opinion.Type;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+
+public class SQLiteWorker {
+
+    private static Connection connect(String dbPath) throws SQLException {
+        return DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+    }
+
+    static ArrayList<Opinion> readDatabase(String dbPath, String dbName)    {
+        ArrayList<Opinion> opinions = new ArrayList<>();
+
+        try ( Connection connection = connect(dbPath);
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM " + dbName.toLowerCase());
+            ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next())    {
+                opinions.add(new Opinion(resultSet.getInt("id"),
+                                        convertDate(resultSet.getString("date")),
+                                        resultSet.getInt("number"),
+                                        Type.valueOf(resultSet.getString("type")),
+                                        resultSet.getInt("weight"),
+                                        resultSet.getString("comment")));
+            }
+
+        }   catch (SQLException e)  {
+            e.printStackTrace();
+        }
+
+        return opinions;
+    }
+
+    public static LocalDate convertDate(String date) {
+        LocalDate output = null;
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            output = LocalDate.parse(date, formatter);
+
+        } catch (DateTimeParseException e) {
+            System.out.println("Parsing error");
+        }
+        return output;
+    }
+
+}
--- Laboratorium_3/src/main/java/org/example/Main.java
+++ Laboratorium_3/src/main/java/org/example/Main.java
@@ -4,11 +4,12 @@ import logic.CompanyFeedback;
 import ui.CMDInterface;
 
 import java.io.IOException;
-
+//C:/Users/agnie/IdeaProjects/Laboratorium_3/Laboratorium_3/src/main/resources/Lab3.db
+//Lab3
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        String dbPath = "C:/Users/agnie/IdeaProjects/Laboratorium_3/Laboratorium_3/src/main/resources/Lab3.db";
-        String dbName = "Lab3";
+        String dbPath = args[0];
+        String dbName = args[1];
         CompanyFeedback feedback = new CompanyFeedback(dbPath, dbName);
         feedback.displayOpinion(15);
         CMDInterface CMDin = new CMDInterface(feedback);
